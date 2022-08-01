3b1fd5ab802114e7f551eb4a2ffe0b1ec3cc3526
preiter <preiter@gwdg.de>
2022-08-01 13:01:43 +0200

    Add: Preconditioner & Error Handling

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,13 @@
 //! <a href="https://github.com/preiter93/gmres/blob/master/doc/gmres.pdf" target="_blank">Algorithm</a>
 //!
 //! # TODO
-//! - Preconditioner
+//! - Preconditioner : Done
 //! - Error Handling
-//! - Test complex values
+//! - Test complex values: Done
+//! - Use relative residual
 use ndarray::{azip, s, Array1, Array2, ArrayBase, DataMut, Ix1};
 use ndarray_linalg::{
-    krylov::AppendResult, krylov::Orthogonalizer, krylov::MGS, norm::Norm,
+    error::Result, krylov::AppendResult, krylov::Orthogonalizer, krylov::MGS, norm::Norm,
     operator::LinearOperator, types::Scalar, Lapack, SolveTriangular,
 };
 use ndarray_linalg::{Diag, UPLO};
@@ -25,31 +26,19 @@ use std::iter::Iterator;
 /// X-vector
 type X<A> = Array1<A>;
 
-/// Residual vector
-type Residual<A> = Vec<<A as Scalar>::Real>;
+/// Residual
+type R<A> = <A as Scalar>::Real;
 
-/// Gmres Result
-pub enum GmresResult<A: Scalar> {
-    Converged((X<A>, Residual<A>)),
-    NotConverged((X<A>, Residual<A>)),
-    Error,
+/// Gmres Status
+#[derive(Clone, Debug)]
+pub enum GmresStatus {
+    /// Gmres converged
+    Converged,
+    /// Gmres not converged
+    NotConverged,
+    /// Krylov vectors are linearly dependent
+    KrylovDependent,
 }
-
-impl<A: Scalar> GmresResult<A> {
-    /// Returns the Gmres result, consuming the `self` value.
-    ///
-    /// # Panics
-    /// Panics if `GmresResult` returned with an error.
-    #[must_use]
-    pub fn unwrap(self) -> (X<A>, Residual<A>) {
-        match self {
-            GmresResult::Converged(val) | GmresResult::NotConverged(val) => val,
-            GmresResult::Error => panic!("Gmres returned with errors. No solution was found."),
-        }
-    }
-}
-
-// pub type Result<T> = ::std::result::Result<T, GmresError>;
 
 // Gmres iterator
 pub struct Gmres<'a, A, S, F, Ortho>
@@ -82,7 +71,11 @@ where
     /// Sine component of Givens matrix
     sn: Vec<A>,
     /// Residual
-    e: Vec<A::Real>,
+    residual: A::Real,
+    /// Preconditioner (optional)
+    pc: Option<F>,
+    /// Status
+    status: GmresStatus,
 }
 
 impl<'a, A, S, F, Ortho> Gmres<'a, A, S, F, Ortho>
@@ -97,23 +90,22 @@ where
     /// # Panics
     /// - `maxiter` > `b.len()`
     #[allow(clippy::many_single_char_names)]
-    pub fn new(
-        a: &'a F,
-        b: &ArrayBase<S, Ix1>,
-        x0: ArrayBase<S, Ix1>,
-        mut ortho: Ortho,
-        // maxiter: usize,
-        // tol: A::Real,
-    ) -> Self {
+    pub fn new(a: &'a F, b: &ArrayBase<S, Ix1>, x0: ArrayBase<S, Ix1>, mut ortho: Ortho) -> Self {
         assert_eq!(ortho.len(), 0);
         assert!(ortho.tolerance() < One::one());
-        // assert!(maxiter <= b.len());
         // First Krylov vector
         let mut v = b - a.apply(&x0);
         // normalize before append
         let norm = v.norm_l2();
         azip!((v in &mut v)  *v = v.div_real(norm));
         ortho.append(v.view());
+        let tol = <A>::real(1e-8_f32);
+        let residual = norm;
+        let status = if residual <= tol {
+            GmresStatus::Converged
+        } else {
+            GmresStatus::NotConverged
+        };
 
         Gmres {
             a,
@@ -122,27 +114,46 @@ where
             ortho,
             m: 0,
             maxiter: b.len(),
-            tol: <A>::real(1e-8_f32),
+            tol,
             r: vec![],
             g: vec![A::from(norm).unwrap()],
             cs: vec![],
             sn: vec![],
-            e: vec![norm],
+            residual,
+            pc: None,
+            status,
         }
     }
 
     /// Set Maximum number of iterations
     #[must_use]
     pub fn maxiter(mut self, maxiter: usize) -> Self {
-        // assert!(maxiter <= self.dim());
         self.maxiter = maxiter;
         self
     }
 
     /// Set convergence tolerance
     #[must_use]
-    pub fn tol(mut self, tol: A::Real) -> Self {
+    pub fn tolerance(mut self, tol: A::Real) -> Self {
+        // Update status
+        self.status = match self.status {
+            GmresStatus::Converged | GmresStatus::NotConverged => {
+                if self.residual <= tol {
+                    GmresStatus::Converged
+                } else {
+                    GmresStatus::NotConverged
+                }
+            }
+            GmresStatus::KrylovDependent => GmresStatus::KrylovDependent,
+        };
         self.tol = tol;
+        self
+    }
+
+    /// Set preconditioner
+    #[must_use]
+    pub fn preconditioner(mut self, pc: F) -> Self {
+        self.pc = Some(pc);
         self
     }
 
@@ -157,13 +168,23 @@ where
     }
 
     /// Return residual
-    pub fn residual(&self) -> Vec<A::Real> {
-        self.e.clone()
+    pub fn residual(&self) -> R<A> {
+        self.residual
+    }
+
+    /// Return current iteration numer
+    pub fn iteration(&self) -> usize {
+        self.m
+    }
+
+    /// Return status
+    pub fn status(&self) -> GmresStatus {
+        self.status.clone()
     }
 
     /// Calculate the givens rotation
     /// [ cs        sn] [ f ]   [ r ]
-    /// [-conj(sn)  cs] [ g ] = [ 0 ]
+    /// [-sn        cs] [ g ] = [ 0 ]
     ///
     /// # Parameters
     /// `f`: Scalar
@@ -219,11 +240,25 @@ where
 
     /// Iterate until convergent
     ///
-    /// # Panics
-    /// - Fail of triangular solve
-    pub fn complete(mut self) -> GmresResult<A> {
-        // Iterate until completion
-        for _ in &mut self {}
+    /// Returns result and vector of residuals
+    ///
+    /// # Errors
+    /// - `solve_triangular` fails
+    pub fn complete(mut self) -> Result<(X<A>, Vec<R<A>>)> {
+        // Iterate until completion and collect residual
+        let mut residuals = vec![self.residual];
+        for r in &mut self {
+            residuals.push(r);
+        }
+        let x = self.finalize()?;
+        Ok((x, residuals))
+    }
+
+    /// Finalize iterator, return solution and residual
+    ///
+    /// # Errors
+    /// - `solve_triangular` fails
+    pub fn finalize(mut self) -> Result<X<A>> {
         // min |g − R y| for y, where R is upper triangular
         let mut r: Array2<A> = Array2::zeros((self.m, self.m));
         for (j, col) in self.r.iter().enumerate() {
@@ -232,20 +267,18 @@ where
             }
         }
         self.g.pop();
-        let g = Array1::from_vec(self.g.clone());
-        match r.solve_triangular(UPLO::Upper, Diag::NonUnit, &g) {
-            Ok(y) => {
-                // Update x = x0 + Q y
-                let x = &self.x0 + &self.ortho.get_q().dot(&y);
-                let residual = self.residual();
-                if residual[residual.len() - 1] <= self.tol {
-                    GmresResult::Converged((x, residual))
-                } else {
-                    GmresResult::NotConverged((x, residual))
-                }
-            }
-            Err(_) => GmresResult::Error,
+        let y = r.solve_triangular(
+            UPLO::Upper,
+            Diag::NonUnit,
+            &Array1::from_vec(self.g.clone()),
+        )?;
+        // Update x = x0 + z
+        let mut z = self.ortho.get_q().dot(&y);
+        if let Some(pc) = &self.pc {
+            pc.apply_mut(&mut z);
         }
+        let x = &self.x0 + &z;
+        Ok(x)
     }
 }
 
@@ -259,29 +292,33 @@ where
     type Item = A::Real;
 
     fn next(&mut self) -> Option<Self::Item> {
+        match self.status {
+            GmresStatus::Converged | GmresStatus::KrylovDependent => return None,
+            GmresStatus::NotConverged => (),
+        };
         // Maximum number of iterations reached
         if self.m >= self.maxiter {
-            return None;
-        }
-        // Convergence limit has been reached
-        if self.e[self.e.len() - 1] <= self.tol {
-            return None;
-        }
-        // Krylov vectors are linearly dependent
-        if self.m > 0 && self.r[self.r.len() - 1][self.m - 1].abs() < self.ortho.tolerance() {
             return None;
         }
 
         // Number of current iteration
         let j = self.m;
+
         // (1) Generate new Krylov vector
+        if let Some(pc) = &self.pc {
+            pc.apply_mut(&mut self.v);
+        }
         self.a.apply_mut(&mut self.v);
         let result = self.ortho.div_append(&mut self.v);
         let norm = self.v.norm_l2();
         azip!((v in &mut self.v) *v = v.div_real(norm));
         // If dependent, it is catched in next iteration
         let mut h = match result {
-            AppendResult::Added(coef) | AppendResult::Dependent(coef) => coef,
+            AppendResult::Added(coef) => coef,
+            AppendResult::Dependent(coef) => {
+                self.status = GmresStatus::KrylovDependent;
+                coef
+            }
         };
 
         // (2) Apply Givens rotation
@@ -293,10 +330,12 @@ where
         self.g[j] = self.cs[j] * self.g[j];
 
         // (3) Check residual
-        let err = self.g[self.g.len() - 1].abs();
-        self.e.push(err);
+        self.residual = self.g[self.g.len() - 1].abs();
+        if self.residual <= self.tol {
+            self.status = GmresStatus::Converged;
+        };
         self.m += 1;
-        Some(err)
+        Some(self.residual)
     }
 }
 
@@ -311,6 +350,9 @@ where
 /// `maxiter`     : Maximum number of Gmres iterations
 /// `tol_mgs`     : Convergence tolerance for Gram-Schmidth orthogonalizer
 /// `tol_gmres`   : Convergence tolerance for Gmres residual
+///
+/// # Errors
+/// - `solve_triangular` fails
 pub fn gmres_mgs<'a, A, S, F>(
     a: &'a F,
     b: &ArrayBase<S, Ix1>,
@@ -318,7 +360,7 @@ pub fn gmres_mgs<'a, A, S, F>(
     maxiter: usize,
     tol_mgs: A::Real,
     tol_gmres: A::Real,
-) -> GmresResult<A>
+) -> Result<(X<A>, Vec<R<A>>)>
 where
     A: Scalar + Lapack,
     S: DataMut<Elem = A>,
@@ -327,7 +369,7 @@ where
     let mgs = MGS::new(b.len(), tol_mgs);
     Gmres::new(a, b, x0, mgs)
         .maxiter(maxiter)
-        .tol(tol_gmres)
+        .tolerance(tol_gmres)
         .complete()
 }
 
@@ -335,7 +377,9 @@ where
 mod test {
     //! cargo test -- --show-output
     use super::*;
-    use ndarray::{array, Data, Dimension};
+    use ndarray::Zip;
+    use ndarray::{Data, Dimension};
+    use ndarray_linalg::types::c64;
     use ndarray_rand::rand_distr::Uniform;
     use ndarray_rand::RandomExt;
 
@@ -356,19 +400,8 @@ mod test {
     }
 
     #[test]
-    fn test1() {
-        let a = array![[1., 2., 3.], [3., 4., 5.], [4., 7., 8.]];
-        let b = array![3., 2., 7.];
-        let x0: Array1<f64> = Array1::zeros(b.len());
-        let maxiter = b.len();
-        let (x, e) = gmres_mgs(&a, &b, x0, maxiter, 1e-8, 1e-8).unwrap();
-        let (size, iter, residual) = (b.len(), e.len() - 1, e[e.len() - 1]);
-        println!("Size {:?} | iter {:?} | Res {:4.2e}", size, iter, residual);
-        approx_eq(&b, &a.dot(&x));
-    }
-
-    #[test]
-    fn test2() {
+    /// Variable size, random input
+    fn test_real() {
         let m = 28;
         let a = Array2::<f64>::random((m, m), Uniform::new(0., 10.));
         let b = Array1::<f64>::random(m, Uniform::new(0., 10.));
@@ -381,16 +414,82 @@ mod test {
     }
 
     #[test]
-    /// a is linearly dependent, test should not converge
-    fn test3() {
-        let a = array![[1., 1., 1.], [1., 1., 1.], [1., 1., 1.]];
-        let b = array![3., 2., 7.];
-        let x0: Array1<f64> = Array1::zeros(b.len());
+    /// Test with complex arrays
+    fn test_gmres_complex() {
+        let m = 28;
+        let a = array2_rand_complex(m, m);
+        let b = array1_rand_complex(m);
+        let x0: Array1<c64> = Array1::zeros(b.len());
         let maxiter = b.len();
-        let result = gmres_mgs(&a, &b, x0, maxiter, 1e-8, 1e-8);
-        match result {
-            GmresResult::Converged(_) | GmresResult::Error => panic!(),
-            GmresResult::NotConverged(_) => (),
-        }
+        let (x, e) = gmres_mgs(&a, &b, x0, maxiter, 1e-8, 1e-8).unwrap();
+        let (size, iter, residual) = (b.len(), e.len() - 1, e[e.len() - 1]);
+        println!("Size {:?} | iter {:?} | Res {:4.2e}", size, iter, residual);
+        approx_eq(&b, &a.dot(&x));
+    }
+
+    fn array2_rand_complex(m: usize, n: usize) -> Array2<c64> {
+        let re = Array2::<f64>::random((m, n), Uniform::new(-1.0, 1.0));
+        let im = Array2::<f64>::random((m, n), Uniform::new(-1.0, 1.0));
+        let mut c = Array2::<c64>::zeros((m, n));
+        Zip::from(&mut c).and(&re).and(&im).for_each(|c, &re, &im| {
+            *c = c64 { re, im };
+        });
+        c
+    }
+
+    fn array1_rand_complex(n: usize) -> Array1<c64> {
+        let re = Array1::<f64>::random(n, Uniform::new(-1.0, 1.0));
+        let im = Array1::<f64>::random(n, Uniform::new(-1.0, 1.0));
+        let mut c = Array1::<c64>::zeros(n);
+        Zip::from(&mut c).and(&re).and(&im).for_each(|c, &re, &im| {
+            *c = c64 { re, im };
+        });
+        c
     }
 }
+
+// #[test]
+// /// Fixed size, fixed input
+// fn test1() {
+//     let a = array![[1., 2., 3.], [3., 4., 5.], [4., 7., 8.]];
+//     let b = array![3., 2., 7.];
+//     let x0: Array1<f64> = Array1::zeros(b.len());
+//     let maxiter = b.len();
+//     let (x, e) = gmres_mgs(&a, &b, x0, maxiter, 1e-8, 1e-8).unwrap();
+//     let (size, iter, residual) = (b.len(), e.len() - 1, e[e.len() - 1]);
+//     println!("Size {:?} | iter {:?} | Res {:4.2e}", size, iter, residual);
+//     approx_eq(&b, &a.dot(&x));
+// }
+
+// /// Finalize iterator, return solution and residual
+// pub fn finalize(mut self) -> GmresResult<A> {
+//     // min |g − R y| for y, where R is upper triangular
+//     let mut r: Array2<A> = Array2::zeros((self.m, self.m));
+//     for (j, col) in self.r.iter().enumerate() {
+//         for (i, v) in col.iter().enumerate() {
+//             r[[i, j]] = *v;
+//         }
+//     }
+//     self.g.pop();
+//     match r.solve_triangular(
+//         UPLO::Upper,
+//         Diag::NonUnit,
+//         &Array1::from_vec(self.g.clone()),
+//     ) {
+//         // Update x = x0 + z
+//         Ok(y) => {
+//             let mut z = self.ortho.get_q().dot(&y);
+//             if let Some(pc) = &self.pc {
+//                 pc.apply_mut(&mut z);
+//             }
+//             let x = &self.x0 + &z;
+//             let residual = self.residual();
+//             if residual[residual.len() - 1] <= self.tol {
+//                 GmresResult::Converged((x, residual))
+//             } else {
+//                 GmresResult::NotConverged((x, residual))
+//             }
+//         }
+//         Err(_) => GmresResult::Error,
+//     }
+// }
